813551dc33ba5302f61a85eff1d7edf80c42bddd
daniel tamayo <danieltamayo1978@gmail.com>
2021-06-09 01:51:08 -0400

    chart imgs added

---
+++ src/components/MultExpansionPanel.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-expansion-panels 
+        accordion
+    >
+        <v-expansion-panel
+            v-for="(multChartImg, i) in multChartImgs"
+            :key="i"
+        >
+            <v-expansion-panel-header class="text-h5 secondary--text font-weight-bold">
+                {{ multChartImg.name }}
+            </v-expansion-panel-header>
+            <v-expansion-panel-content class="pt-4">
+                <v-img
+                    :src="imgChartsFolder + multChartImg.img"
+                    width="100%"
+                ></v-img>
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+    </v-expansion-panels>
+
+</template>
+
+<script>
+  export default {
+    name: 'MultExpansionPanel',
+    props: {
+      multChartImgs: Array,
+    },
+
+    data: () => ({
+        imgChartsFolder: process.env.VUE_APP_IMG_CHARTS_FOLDER,
+    }),
+
+    methods: {
+    }
+
+  }
+</script>
